8fc47198df23a72186f6ed730e7ebbec38a50ab7
Artem <mav.artem.kz2007@gmail.com>
2026-05-17 23:14:09 +0000

    test deploy

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ task_id = 0
 
 @router.get("/")
 def root():
-    return {"message": "Service is running after CI/CD deploy, mod 4"}
+    return {"message": "Service is running after CI/CD deploy, mod 5"}
 
 
 @router.post("/tasks")
